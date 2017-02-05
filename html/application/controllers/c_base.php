fc5f9a8b7014847f1ee0c7198c9cc1fc24c3bfe9
rintaro.yamasaki <r_y1107@yahoo.co.jp>
2017-02-05 19:42:33 +0900

    初回リモートアップ

---
+++ html/application/controllers/c_base.php
@@ -0,0 +1,14 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class C_base extends CI_Controller {
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	function index()
+	{
+		$this->load->view('welcome_message');
+	}
+}
